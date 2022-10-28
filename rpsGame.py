8529ea328ddfec0ae9c4442ce97e0eb74fa78804
Messites <revnaweji@gmail.com>
2022-10-28 13:37:47 -0500

    Update rpsGame.py

--- rpsGame.py
+++ rpsGame.py
@@ -2,12 +2,12 @@ import random, sys
 
 print('ROCK, PAPER, SCISSORS')
 
-# These variables keep track of the number of wins, losses, and ties. 
+# These variables will keep track of the number of the wins, losses, and ties. 
 wins = 0
 losses = 0
 ties = 0
 
-while True: # The main game loop. 
+while True: # This section is the main game loop. 
     print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
     while True: # The Player input loop.
         print('Enter your move: (r)ock (p)aper (s)cissors orr (q)uit')
@@ -18,13 +18,13 @@ while True: # The main game loop.
             break # Break out of the player input look. 
         print('Type one of r, p, s, or q.')
         
-    # Display what the player chose:
+    # This section displays what the player chose:
     if playerMove == 'r':
         print('ROCK versus...')
     elif playerMove == 'p':
         print('SCISSORS versus...')
         
-    # Display what the computer chose: 
+    # This section will display what the computer chose: 
     randomNumber = random.randint(1, 3)
     if randomNumber == 1:
         computerMove = 'r'
@@ -36,7 +36,7 @@ while True: # The main game loop.
         computerMove = 's'
         print('SISCORS')
         
-    # Display and record the win/loss/tie:
+    # This section will display and record the wins/losss/ties:
     if playerMove == computerMove:
         print('It is a tie!')
         ties = ties + 1
@@ -57,4 +57,6 @@ while True: # The main game loop.
     elif playerMove == 's' and computerMove == 'r':
         print('You lose!')
         losses = losses + 1
+        
+   # Enjoy the game!!!!
     
